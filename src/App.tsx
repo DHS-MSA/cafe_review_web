88704941f4daac305fbaf04422f4ef75e82249a9
chaesuhyeon <ddd8177@gmail.com>
2022-11-12 11:47:16 +0900

    react project init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      메인페이지
+    </div>
+  );
+}
+
+export default App;
