bf5cc6579a09b5260af6992c30b4fbc9701a5f06
nohyunha95 <nohyunha95@gmail.com>
2022-11-27 22:03:30 +0900

    FEAT: 헤더 & 컨텐츠 컴포넌트 구현 중

--- src/components/layout/Contents.tsx
+++ src/components/layout/Contents.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { Margin } from "@mui/icons-material";
+
 function Contents() {
-  return;
+  const [alignment, setAlignment] = useState<string>("web");
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
+    setAlignment(newAlignment);
+  };
+
+  return (
+    <>
+      <ToggleButtonGroup
+        color="primary"
+        value={alignment}
+        exclusive
+        size="large"
+        onChange={handleChange}
+        aria-label="Platform"
+        style={{ display: "flex", justifyContent: "center" }}
+      >
+        <ToggleButton value="web">전체</ToggleButton>
+        <ToggleButton value="android">체인점</ToggleButton>
+        <ToggleButton value="ios">개인카페</ToggleButton>
+      </ToggleButtonGroup>
+    </>
+  );
 }
 
 export default Contents;
