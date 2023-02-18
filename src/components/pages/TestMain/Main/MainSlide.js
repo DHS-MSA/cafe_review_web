64eed4e688264df0233519378767beee3c0b5916
kyukim <kyukim@student.42seoul.kr>
2023-02-18 21:06:51 +0900

    [DELETE] image.identifier

--- src/components/pages/TestMain/Main/MainSlide.js
+++ src/components/pages/TestMain/Main/MainSlide.js
@@ -5,18 +5,18 @@ const MainSlide = (props) => {
 	const storeInfos = props.storeInfos;
 
 	return <div className={styles.container}>
-		<button className={styles.arrow}>{"<"}</button>
-		{/* <ul className={styles["slide-show"]}>
-			<li className={styles.item}><img src='images/choki.jpg'></img></li>
-			<li className={styles.item}><img src='images/ban_nim_2.jpg'></img></li>
-			<li className={styles.item}><img src='images/natsue.jpg'></img></li>
-		</ul> */}
-		<ul className={styles["slide-show"]}>
-			{storeInfos.map((storeInfo)=>{
-				return <li className={styles.item}><MainItem storeInfo={storeInfo}></MainItem></li>
-			})}
-		</ul>
-		<button className={styles.arrow}>{">"}</button>
+		<div className={styles['sub-container']}>
+			<p className={styles['slide-tag']}> Tag</p>
+			<div className={styles['slide-container']}>
+				<button className={styles.arrow}>{"<"}</button>
+				<ul className={styles["slide-show"]}>
+					{storeInfos.map((storeInfo, index)=>{
+						return <li key={index} className={styles.item}><MainItem storeInfo={storeInfo}></MainItem></li>
+					})}
+				</ul>
+				<button className={styles.arrow}>{">"}</button>
+			</div>
+		</div>
 	</div>
 };
 
--- src/components/pages/TestMain/TestPage.js
+++ src/components/pages/TestMain/TestPage.js
@@ -3,14 +3,12 @@ import './TestPage.css';
 import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
-import MainItem from "./Main/MainItem";
 
 const TestPage = () => {
 	return <div className="wrap">
 		<Header />
 		<Main />
 		<Footer />
-		{/* <MainItem /> */}
 	</div>
 };
 
